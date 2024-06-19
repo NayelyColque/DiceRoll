af31cff6dc84dd7dacc8ed5647c11d310409c2b4
NayelyColque <nayelycolquemamani@gmail.com>
2024-06-18 23:51:36 -0300

    feat: Initial commit

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int numeroSorteado = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            SidesNumbersPicker.SelectedIndex = 0;
+        }
+        private void RollDiceButton_Clicked(object sender, EventArgs e)
+        {
+           
+            int max = Convert.ToInt32(SidesNumbersPicker.SelectedItem);
+            numeroSorteado = new Random().Next(1, max + 1);
+            DiceResultLabel.Text = numeroSorteado.ToString();
+        }
+
+
+    }
+
+}
